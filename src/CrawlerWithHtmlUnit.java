db47f8a3d35ce4124fbfb992bdd53eb72e715329
Manish Singh <mrsingh@ncsu.edu>
2015-02-17 22:51:34 -0500

    Added next page feature

--- src/CrawlerWithHtmlUnit.java
+++ src/CrawlerWithHtmlUnit.java
@@ -24,11 +24,11 @@ public class CrawlerWithHtmlUnit {
 
 		BufferedReader readHandle;
 
-		File file = new File("/home/manish/workspace/GitCrawler/CrawlerOutput.txt"); //We write into this file
+		File file = new File("/home/manish/workspace/crawl-repo/CrawlerOutput.txt"); //We write into this file
 
 		try {
 			
-			readHandle = new BufferedReader(new FileReader("/home/manish/workspace/GitCrawler/InputPolicyKeywords.txt"));  //Input file containing keywords
+			readHandle = new BufferedReader(new FileReader("/home/manish/workspace/crawl-repo/InputPolicyKeywords.txt"));  //Input file containing keywords
 
 			String keywordLine; // this will contain each of the keywords as the program reads line by line
 
@@ -87,7 +87,7 @@ public class CrawlerWithHtmlUnit {
 							System.out.println("size of urls is:"+urlsOfFileCommits.size());
 						}
 						
-						for(String url : urlsOfFileCommits){
+						for(String url : urlsOfFileCommits){//match1
 							driver.get(url);
 							List<WebElement> commitsPerFile = driver.findElements(By.xpath("//*[@id=\"compare\"]/div[2]/ol/li/div[2]/p/a"));
 							
@@ -109,7 +109,7 @@ public class CrawlerWithHtmlUnit {
 								
 //								String search="//td[(contains(@class,'blob-code blob-code-addition') and contains(.,'HasRole')) or (contains(@class,'blob-code blob-code-deletion') and contains(.,'HasRole'))]";
 								
-								String search="//td[(contains(@class,'blob-code blob-code-addition') and contains(.,'"+keywordLine+"')) or (contains(@class,'blob-code blob-code-deletion') and contains(.,'"+keywordLine+"'))]";
+								String search="//td[(contains((@class,'blob-code blob-code-addition') and contains(.,'"+keywordLine+"')) or (contains(@class,'blob-code blob-code-deletion') and contains(.,'"+keywordLine+"'))]";
 								
 								System.out.println("SEARCH STRING IS -->"+search);
 								List<WebElement> listChanges = driver.findElements(By.xpath(search));
@@ -151,199 +151,19 @@ public class CrawlerWithHtmlUnit {
 							}
 							
 							
-						}
+						}//match1
 						
-						System.out.println("ABOUT TO START SLEEPING +++++++++++++++++++++++++++++++");
-						Thread.sleep(1000L);
-						
-						
-						By getChanges = By.xpath("//td[contains(@class,'blob-code blob-code-addition') or contains(@class,'blob-code blob-code-deletion')]");
-						
-//						String getCount = By
-//								.xpath(count("//td[contains(@class,'blob-code blob-code-addition') or contains(@class,'blob-code blob-code-deletion')]"));
-
-						List<WebElement> listChanges = driver.findElements(getChanges);
-
-						System.out.println("ListChanges size is :"+ listChanges.size());
-
-						if (listChanges.size() < 100)
-						{
-															
-							for (int count = 0; count < listChanges.size(); count++) {
-
-								// System.out.println(driver.findElements(getChanges).get(count).getText());
-
-								// codeChanges=driver.findElements(getChanges).get(count).getText();
-
-								//prints all the code changes associated with each commit
+						try{
+								String nextPageUrl = driver.findElement(By.className("next_page")).getAttribute("href");
+								driver.get(nextPageUrl);
 								
-								String codeChanges = listChanges.get(count).getText();
-
-								// System.out.println("Writing code changes into file now *********");
-
-								outputHandle.write(codeChanges);
-
-								outputHandle.write("\n");
-
-							}
-							
-						}//This is end of if condition for listChanges.size
-						
-						
-						Thread.sleep(10000L);
-						
-						List<WebElement> commitsPerFile = driver.findElements(By.xpath("//a[contains(@class,'sha button-outline')]"));
-						
-						List<String> urlsOfcommitsPerFile = new LinkedList<String>();
-						
-						//Extract the URLS from each of the WebElement and put it in list urlsOfcommitsPerFile
-						for(WebElement result : commitsPerFile){
-							urlsOfcommitsPerFile.add(result.getAttribute("href"));
-							System.out.println("size of urls is:"+urlsOfFileCommits.size());
-							
-						}
-						
-						
-						
-						element = driver.findElement(By.className("code-list-item")); // check // if // // this // line // // is // // required
-
-						By selector = By.xpath("//p[@class='title']/a[2]"); // returns // all // the // classes // with // title // on // search // results // page
-
-						// System.out.println(selector);
-
-						List<WebElement> list = driver.findElements(selector);
-
-						 System.out.println("List size is :" + list.size());
-
-						// For each of the search results
-						for (int i = 0; i < list.size(); i++) {
-
-							new WebDriverWait(driver, 10)
-							.until(ExpectedConditions
-									.elementToBeClickable(selector));
-
-							// System.out.println(driver.findElements(selector).get(i).getText());
-
-							// For each file , we write the below header to
-							// differentiate the code changes w.r.t files
-							outputHandle
-							.write("======================================================\n");
-							outputHandle.write("[OUTPUT-FILENAME]: "
-									+ driver.findElements(selector).get(i)
-									.getText()); // write the file which // is being // processed into // the output file
-							outputHandle
-							.write("\n======================================================");
-
-							outputHandle.write("\n\n");
-
-							// Click on the title
-							driver.findElements(selector).get(i).click();
-
-							// Thread.sleep(4000L);
-
-							element =driver.findElement(By.xpath("//a[contains(@class,'minibutton') and .//text()='History']"));
-
-							element.click();
-
-							By getCommitList = By.xpath("//a[contains(@class,'sha button-outline')]");
-
-							List<WebElement> commitList = driver
-									.findElements(getCommitList);
-
-							 System.out.println("CommitList size is :" +commitList.size());
-
-							for (int commitCount = 0; commitCount < commitList.size(); commitCount++) {
-								System.out.println("current commitCount:"+commitCount);
-								String commitName = driver
-										.findElements(getCommitList)
-										.get(commitCount).getText(); // works
-								// but
-								// redundant
-								// call
-
-								// String
-								// commitName=commitList.get(commitCount).getText();
-
-								System.out.println(commitName);
-
-								// commitList.get(commitCount).click();
-								// //suggested improvement but fails
-
-								driver.findElements(getCommitList)
-								.get(commitCount).click();
-
-//								By getChanges = By.xpath("//td[contains(@class,'blob-code blob-code-addition') or contains(@class,'blob-code blob-code-deletion')]");
-								
-//								String getCount = By
-//										.xpath(count("//td[contains(@class,'blob-code blob-code-addition') or contains(@class,'blob-code blob-code-deletion')]"));
-
-//								List<WebElement> listChanges = driver.findElements(getChanges);
-
-								System.out.println("ListChanges size is :"+ listChanges.size());
-
-								if (listChanges.size() < 100)
-								{
-																	
-									for (int count = 0; count < listChanges.size(); count++) {
-	
-										// System.out.println(driver.findElements(getChanges).get(count).getText());
-
-										// codeChanges=driver.findElements(getChanges).get(count).getText();
-	
-										//prints all the code changes associated with each commit
-										
-										String codeChanges = listChanges.get(count).getText();
-	
-										// System.out.println("Writing code changes into file now *********");
-	
-										outputHandle.write(codeChanges);
-	
-										outputHandle.write("\n");
-	
-									}
-									
-								}//This is end of if condition for listChanges.size
-								
-								// AFTER FINISHING FIRST COMMIT, YOU MOVE TO THE NEXT COMMIT ON PREVIOUS PAGE
-								
-								driver.navigate().back();
-
-							}// end of commit count loop
-
-							// After you have covered all the commits w.r.t a single file, go back to the previous page - which is that file page.
-
-							driver.navigate().back();
-
-							// outputHandle.close(); // This should not be here.
-							// To be closed after processing all the search results
-
-							// System.out.println(textString);
-
-							// Thread.sleep(4000L);
-
-							// This takes you to the search results page. From here we click on the next file in the next iteration
-							
-							driver.navigate().back(); // you can also consider storing the url of results page and going directly there
-							
+						}catch(Exception e){
+							e.printStackTrace();
+							break;
 						}
 
-						element = driver.findElement(By.className("code-list-item"));  // looks like this is not required. to be verified later
-
-						try {
-								System.out.println("Counter value is :"+counter);
-								
-								element = driver.findElement(By.xpath("//a[contains(@rel,'next')]"));
-
-								element.click();
-							
-						} catch (Exception e) {
-							
-							System.out.println("Inside break");
-							
-							break; // In case the fetch fails , come out of the infinite while loop
-						}
-
-					}// Close the infinite while loop
+				
+					}// Close the infinite while loop 
 
 					outputHandle.close(); 
 					
