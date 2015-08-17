c2bef80be41c9cd59b621c54118fe5ff4cbde763
Manish Singh <mrsingh@ncsu.edu>
2015-08-17 12:49:42 -0400

    This commit is being made by scheduled job

--- job.sh
+++ job.sh
@@ -1,10 +1,14 @@
 
-git status;
+while true
+do
+	sleep 1m;
+	git status;
+	git add -A;
+	git commit -m "This commit is being made by scheduled job";
+	git push origin master;
 
-git add -A ;
+done
 
-git commit -m "This commit is being made by scheduled job."
 
-git push origin master
 
 
