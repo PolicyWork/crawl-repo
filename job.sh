90620088e73b1dbbb35c3b5ccd59ff8564375377
Manish Singh <mrsingh@ncsu.edu>
2015-08-17 12:55:00 -0400

    This commit is being made by scheduled job

--- job.sh
+++ job.sh
@@ -6,7 +6,8 @@ do
 	git add -A;
 	git commit -m "This commit is being made by scheduled job";
 	git push origin master;
-
+       echo "commit done at"+`date` >> job.log;
+	echo "=================================";
 done
 
 
