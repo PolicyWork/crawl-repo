1b4426f79ce9fe3c4daaf6fbd07bb6396b12e88e
Manish Singh <mrsingh@ncsu.edu>
2015-08-18 10:30:54 -0400

    This commit is being made by scheduled job

--- OUTPUT/EXCELFILLER/run_postfilter.sh
+++ OUTPUT/EXCELFILLER/run_postfilter.sh
@@ -6,11 +6,11 @@ echo "execution will start now...\n\n\n"
 
 
 cd /home/manish/workspace/crawl-repo/OUTPUT/POSTFILTER/BEST*
-cat Output* |grep "CODE-CHANGE-URL"|sort -u|cut -d "]" -f2 > /home/manish/workspace/crawl-repo/OUTPUT/EXCELFILLER/LinksToReadPostFilter.txt
+cat Output* |grep "CODE-CHANGE-URL"|sort -u|cut -d "]" -f2 > /home/manish/workspace/crawl-repo/OUTPUT/EXCELFILLER/LinksToReadPostFilter2.txt
 
-perl -pi -e "s/^://g" /home/manish/workspace/crawl-repo/OUTPUT/EXCELFILLER/LinksToReadPostFilter.txt
+perl -pi -e "s/^://g" /home/manish/workspace/crawl-repo/OUTPUT/EXCELFILLER/LinksToReadPostFilter2.txt
 
-
+cd ../../
 
 
 
